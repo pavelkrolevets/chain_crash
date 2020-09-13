4f2751a59c0e1ba4d0b44a48b1bb83e528d54243
pavelkrolevets <pavelkrolevets@gmail.com>
2020-09-13 13:45:00 +0600

    New structure

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pavelkrolevets/chain_crash/src/client
+
+go 1.14
+
+require (
+	github.com/ethereum/go-ethereum v1.9.21
+	gopkg.in/yaml.v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"io/ioutil"
+	"flag"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pavelkrolevets/chain_crash/src/client/src/client"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 )
 
@@ -35,4 +38,40 @@ func (c *Config) getConfig() *Config  {
 func main(){
 	GlobalConfig.getConfig()
 	log.Println(GlobalConfig)
+	// Parse args
+	tps:= flag.Int("tps", 1, "tps speed from the node")
+	test_size := flag.Int("test_size", 60000, "transaction test size")
+	rpc_addr := flag.String("rpc_addr", "http://127.0.0.1:8545", "Node RPC address and open port")
+	check_txs :=  flag.Bool("check_txs", false,"Check if all sent transactions are in the blockchain ")
+	//threads := flag.Int("threads", 4,"Number of threads to run")
+	heavy_io :=  flag.Bool("heavy_io", false,"Add 2048 digit HEX of random data to each tx for heavy io")
+
+	flag.Parse()
+
+	eth_client, err := ethclient.Dial(*rpc_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tx_ch_1, tx_ch_2, tx_ch_3, tx_ch_4 := make(chan string, 1), make(chan string, 1), make(chan string, 1), make(chan string, 1)
+	go client.SendTransactions(eth_client, GlobalConfig.Addr2, GlobalConfig.PK1, tx_ch_1, *tps, *test_size, *check_txs, *heavy_io)
+	go client.SendTransactions(eth_client, GlobalConfig.Addr3, GlobalConfig.PK2, tx_ch_2, *tps, *test_size, *check_txs, *heavy_io)
+	go client.SendTransactions(eth_client, GlobalConfig.Addr4, GlobalConfig.PK3, tx_ch_3, *tps, *test_size, *check_txs, *heavy_io)
+	go client.SendTransactions(eth_client, GlobalConfig.Addr1, GlobalConfig.PK4, tx_ch_4, *tps, *test_size, *check_txs, *heavy_io)
+	result_ch := make(chan string, 1)
+	go client.GetTransactionsBlockCount(eth_client, result_ch)
+
+	for {
+		select {
+		case c1:= <- tx_ch_1:
+			log.Println("Received from ch1", c1)
+		case c2:= <- tx_ch_2:
+			log.Println("Received from ch2", c2)
+		case c3:= <- tx_ch_3:
+			log.Println("Received from ch3", c3)
+		case c4:= <- tx_ch_4:
+			log.Println("Received from ch4", c4)
+		case c5:= <- result_ch:
+			log.Println(c5)
+		}
+	}
 }
